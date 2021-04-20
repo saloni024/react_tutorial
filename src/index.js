d2d4b0b9656baf9f63a39389cd782825bfbf1e4f
saloni024 <saloniprajapati024@gmail.com>
2021-04-20 15:59:30 -0400

    React tutorial

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<h1>Hello world</h1>,document.getElementById("root"))
+//ReactDOM and JSX
+ReactDOM.render(<div><h1>Hello world</h1><p>This is paragraph!</p></div>,document.getElementById("root"))
+
+var newP = document.createElement("p")
+newP.setAttribute("id","newP")
+newP.innerHTML = "This is a new paragraph."
+document.getElementById("root").appendChild(newP)
+
+
+//Functional components
+function MyList(){ //prefer capital camel casing in naming convention
+    return (
+        <div>Series list: 
+            <ul>
+                <li>The office</li>
+                <li>Brooklyn 99</li>
+                <li>Suits</li>
+            </ul>
+        </div>
+    )
+}
+ReactDOM.render(<MyList/>, document.getElementById("newP"))
