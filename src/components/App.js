15de912451d22dd22363361517b386124d8f674f
saloni024 <saloniprajapati024@gmail.com>
2021-04-22 01:00:48 -0400

    React tutorial

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,13 @@
 import React from "react"
+import Navbar from "./Navbar"
+import MainContent from "./MainContent"
 import Footer from "./Footer"
 
 function App(){
     return (
         <div>
-            <nav>
-                <h3>Parent/child nested components</h3>
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ul>
-            </nav>
-            <main>
-                <p>This is where most of the content will go</p>
-            </main>
+            <Navbar />
+            <MainContent />
             <Footer />
         </div>
     )
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Navbar(){
+    return (
+        <nav>
+                <h3>Parent/child nested components</h3>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+        </nav>
+    )
+}
+export default Navbar
